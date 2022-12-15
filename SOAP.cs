b0bb633253dc78bccf0bf0c1861fd7bec15942f1
toqatamer <118900474+toqatamer@users.noreply.github.com>
2022-12-15 21:00:29 +0200

    Add files via upload

---
+++ SOAP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireFrames
+{
+    internal class SOAP
+    {
+        public string PetID { get; set; }
+        public string Subjective { get; set;}
+        public string Objective { get; set;}
+        public string Assessment { get; set;}
+        public string Plan__ { get; set;}
+        public string Date { get; set;}
+    }
+}
---
+++ SignUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireFrames
+{
+    internal class PatientSignUp
+    {
+        public string PatientName { get; set; }
+        public string PatientNationalID { get; set; }
+        public string PatientPassword { get; set; }
+        public string PatientPhone { get; set; }
+    }
+}
